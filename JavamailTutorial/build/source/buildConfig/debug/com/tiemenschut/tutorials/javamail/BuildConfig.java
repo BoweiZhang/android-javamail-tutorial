0ceffdc1a4d8595be7d468a55c66c3d4bb14d9a5
tschut <tschut@gmail.com>
2013-07-04 15:55:54 +0200

    Update .gitignore

--- JavamailTutorial/build/source/buildConfig/debug/com/tiemenschut/tutorials/javamail/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.tiemenschut.tutorials.javamail;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-
-}
